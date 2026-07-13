734016358a2edec9b830a4b93930fdafe6a3dd1c
Hafia Khalid <forw6157@gmail.com>
2026-07-13 22:12:30 +0500

    part3 assignment completed

--- part3.py
+++ part3.py
@@ -1,8 +1,5 @@
 #Q1:Ask the user for a string and check whether it is a palindrome or not. is a string which is same when we read it forward & backward. Eg- "madam", "racecar" etc.
 #[Hint: A palindrome string is equal to its reversed version. We can use a loop to reverse the string manually.]
-from numpy import concatenate
-
-
 user_input = input("Enter a string: ")
 reversed_input = ""
 for char in user_input:
